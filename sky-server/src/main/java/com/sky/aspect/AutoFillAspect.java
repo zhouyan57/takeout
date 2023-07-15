0c7cacc8900dd87e61ae2d3ed87b6d55a8479ec3
Jane <994824764@qq.com>
2023-07-15 15:06:50 +0800

    公共字段填充 - AutoFill中通过反射为公共字段赋值

--- sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -1,12 +1,21 @@
 package com.sky.aspect;
 
+import com.sky.annotation.AutoFill;
+import com.sky.context.BaseContext;
+import com.sky.enumeration.OperationType;
 import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.Joinpoint;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.aspectj.weaver.ast.Var;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
 
 /**
  * ClassName: AutoFillAspect
@@ -38,5 +47,56 @@ public class AutoFillAspect {
     public void autoFill(JoinPoint joinpoint){
         log.info("开始进行公共字段的自动填充...");
         // 传进来的参数是连接点 通过连接点知道 哪个方法被拦截到了 以及方法的参数是样的(参数值、参数类型)
+
+        // 获取到当前被拦截的方法上的数据库操作类型
+        MethodSignature signature = (MethodSignature) joinpoint.getSignature(); // 向下转型 方法签名对象
+        AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class); // 获得方法上的注解对象
+        OperationType operationType = autoFill.value(); // 获得数据库操作类型
+
+        // 获取到当前被拦截的方法上的参数--实体对象
+        Object[] args = joinpoint.getArgs();
+        if(args == null || args.length == 0){
+            return;
+        }
+
+        Object entity = args[0]; // 约定实体放在第一位
+
+        // 准备赋值的数据
+        LocalDateTime now = LocalDateTime.now(); // 当前时间
+        Long currentId = BaseContext.getCurrentId(); // 当前登录用户id
+
+        // 根据当前不同的操作类型，为对应的属性通过反射赋值
+        if(operationType == OperationType.INSERT){
+            // 为4个公共字段赋值
+            try {
+                // 获得set方法
+                Method setCreateTime = entity.getClass().getDeclaredMethod("setCreateTime", LocalDateTime.class);
+                Method setCreateUser = entity.getClass().getDeclaredMethod("setCreateUser", Long.class);
+                Method setUpdateTime = entity.getClass().getDeclaredMethod("setUpdateTime", LocalDateTime.class);
+                Method setUpdateUser = entity.getClass().getDeclaredMethod("setUpdateUser", Long.class);
+
+                // 通过反射来为对象属性赋值
+                setCreateTime.invoke(entity,now);
+                setCreateUser.invoke(entity,currentId);
+                setUpdateTime.invoke(entity,now);
+                setUpdateUser.invoke(entity,currentId);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }else if(operationType == OperationType.UPDATE){
+            // 为2个公共字段赋值
+            try {
+                // 获得set方法
+                Method setUpdateTime = entity.getClass().getDeclaredMethod("setUpdateTime", LocalDateTime.class);
+                Method setUpdateUser = entity.getClass().getDeclaredMethod("setUpdateUser", Long.class);
+
+                // 通过反射来为对象属性赋值
+                setUpdateTime.invoke(entity,now);
+                setUpdateUser.invoke(entity,currentId);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
