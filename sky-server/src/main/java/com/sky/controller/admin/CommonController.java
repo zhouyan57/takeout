0421bfdcbfb4dba31b9ac1f79b55fae51fc35810
Jane <994824764@qq.com>
2023-07-15 20:32:43 +0800

    文件上传 - upload接口完成

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,63 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * ClassName: CommonController
+ * Package: com.sky.controller.admin
+ * Description: 通用接口
+ *
+ * @Author: Jane
+ * @Create: 2023/7/15 - 15:28
+ * @version: v1.0
+ */
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+@Slf4j
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    // 参数：接收的是二进制文件，spring MVC给我们进行了封装 使用MultipartFile类型的对象就行；file跟前端提交的参数名保持一致
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
+    public Result<String> upload(MultipartFile file){
+        log.info("文件上传：{}",file);
+
+        try {
+            // 原始文件名
+            String originalFilename = file.getOriginalFilename();
+            // 截取原始文件名的后缀 fhdi.png
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            // 构造新文件名称
+            String objectName = UUID.randomUUID().toString() + extension;
+
+            // 文件的请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(),objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败：{}", e);
+        }
+
+        return null;
+    }
+}
