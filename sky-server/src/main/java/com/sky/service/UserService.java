f4109e12a0be845e937393c1396c4276ba94bca4
Jane <994824764@qq.com>
2023-07-17 15:24:29 +0800

    微信登录 - 功能完成

---
+++ sky-server/src/main/java/com/sky/service/UserService.java
@@ -0,0 +1,23 @@
+package com.sky.service;
+
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+
+/**
+ * ClassName: UserService
+ * Package: com.sky.service
+ * Description:
+ *
+ * @Author: Jane
+ * @Create: 2023/7/17 - 12:15
+ * @version: v1.0
+ */
+public interface UserService {
+
+    /**
+     * 微信登录
+     * @param userLoginDTO
+     * @return
+     */
+    User wxLogin(UserLoginDTO userLoginDTO);
+}
