029c2ee9109f07da4929f45229139c15a18f4ce4
Jane <994824764@qq.com>
2023-07-21 07:34:51 +0800

    用户端历史订单模块 - 查询历史订单

--- sky-server/src/main/java/com/sky/task/MyTask.java
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -22,8 +22,8 @@ public class MyTask {
      * 定时任务 每隔5秒触发一次
      */
     // 指定任务何时触发 秒 分 小时 日 月 周 年
-    @Scheduled(cron = "0/5 * * * * ?") // 每隔五秒触发一次
-    public void executeTask(){
-        log.info("定时任务开始执行：{}", new Date());
-    }
+//    @Scheduled(cron = "0/5 * * * * ?") // 每隔五秒触发一次
+//    public void executeTask(){
+//        log.info("定时任务开始执行：{}", new Date());
+//    }
 }
