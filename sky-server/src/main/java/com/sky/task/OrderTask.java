f858b2025ea565c7f1f27406eda48747fed631c8
Jane <994824764@qq.com>
2023-07-21 09:34:27 +0800

    定时处理订单状态 - 处理超时订单

---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,51 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * ClassName: OrderTask
+ * Package: com.sky.task
+ * Description: 定时任务类，定时处理订单状态
+ *
+ * @Author: Jane
+ * @Create: 2023/7/21 - 7:02
+ * @version: v1.0
+ */
+@Component // 实例化，并放到Spring容器中管理
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 处理超时订单的方法
+     */
+    @Scheduled(cron = "0 * * * * ? ") // 每分钟触发一次
+    public void processTimeoutOrder(){
+        log.info("定时处理超时订单：{}", LocalDateTime.now());
+
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+
+        // select * from orders where status = ? and order_time < (当前时间 - 15分钟)
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, time);
+
+        if(ordersList != null && ordersList.size() > 0){
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+    }
+}
