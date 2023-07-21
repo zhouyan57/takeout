f3164293ddb21a61acbf4b2c48867323c89ff7e1
Jane <994824764@qq.com>
2023-07-21 20:21:48 +0800

    Apache POI - 入门案例 - 通过POI读取Excel文件内容

--- sky-server/src/test/java/com/sky/test/POITest.java
+++ sky-server/src/test/java/com/sky/test/POITest.java
@@ -5,7 +5,9 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 
 /**
  * ClassName: POITest
@@ -42,7 +44,7 @@ public class POITest {
         row.createCell(2).setCellValue("深圳");
 
         // 通过输出流将内存中的Excel文件写入到磁盘
-        FileOutputStream out = new FileOutputStream(new File("D:\\info.xlsx"));
+        FileOutputStream out = new FileOutputStream(new File("E:\\info.xlsx"));
         excel.write(out);
 
         // 关闭资源
@@ -50,7 +52,40 @@ public class POITest {
         excel.close();
     }
 
+    /**
+     * 通过POI读取Excel文件中的内容
+     * @throws Exception
+     */
+    public static void read() throws Exception{
+
+        // 输入流对象，通过它读取已有的Excel文件
+        InputStream in = new FileInputStream(new File("E:\\info.xlsx"));
+
+        // 读取磁盘上已经存在的Excel文件
+        XSSFWorkbook excel = new XSSFWorkbook(in);
+        // 读取Excel文件中的第一个Sheet页
+        XSSFSheet sheet = excel.getSheetAt(0);
+
+        // 获取Sheet中最后一行的行号(从0开始)
+        int lastRowNum = sheet.getLastRowNum();
+
+        for (int i = 1; i <= lastRowNum; i++) {
+            // 获得某一行
+            XSSFRow row = sheet.getRow(i);
+            // 获得单元格对象
+            String cellValue1 = row.getCell(1).getStringCellValue();
+            String cellValue2 = row.getCell(2).getStringCellValue();
+            System.out.println(cellValue1 + " " + cellValue2);
+        }
+
+        // 关闭资源
+        in.close();
+        excel.close();
+
+    }
+
     public static void main(String[] args) throws Exception {
         write();
+        read();
     }
 }
